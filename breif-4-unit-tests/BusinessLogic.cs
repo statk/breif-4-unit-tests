22af48a1f3eaca2d21aea27eb1d89a09d130eea4
DESKTOP-UJDI90S\Kirill <kirill.stativkin@studentpartner.com>
2018-06-03 21:09:39 +0200

    with out unit tests

---
+++ breif-4-unit-tests/BusinessLogic.cs
@@ -0,0 +1,25 @@
+﻿namespace breif_4_unit_tests
+{
+    public class BusinessLogic
+    {
+        private readonly int _firstNumber;
+        private readonly int _secondNumber;
+
+        public BusinessLogic(int firstNumber, int secondNumber )
+        {
+            _firstNumber = firstNumber;
+            _secondNumber = secondNumber;
+        }
+       
+        public int CalculateSum()
+        {
+            return _firstNumber + _secondNumber;
+        }
+        
+        public int CalculateMultiplication()
+        {
+            return CalculateSum();
+        }
+
+    }
+}
---
+++ breif-4-unit-tests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace breif_4_unit_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var businessLogic = new BusinessLogic(2, 2);
+            var sumResult = businessLogic.CalculateSum();
+            var multipleResult = businessLogic.CalculateMultiplication();
+
+            Console.WriteLine("Какую операцию вывести?: \n" +
+                "1: Сложение; \n" +
+                "2: Умножение;\n");
+            var operationId = int.Parse(Console.ReadLine());
+            switch (operationId)
+            {
+                case 1: Console.WriteLine($"\n Сумма чисел равна {sumResult}"); break;
+                case 2: Console.WriteLine($"\n Произведение чисел равно {sumResult}"); break;
+                default:
+                    Console.WriteLine($"\n Нет такой операции"); break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
